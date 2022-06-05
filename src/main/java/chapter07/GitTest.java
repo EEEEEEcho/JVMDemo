45d38f13d0bd15f291897cf2d8b51017fa7e92e3
EEEEEEcho <814466057@qq.com>
2022-06-05 19:22:24 +0800

    测试提交三

--- src/main/java/chapter07/GitTest.java
+++ src/main/java/chapter07/GitTest.java
@@ -3,5 +3,7 @@ package chapter07;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git1");
+        System.out.println("Hello git2");
+
     }
 }
