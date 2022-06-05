447b598aff097eb24c05f474c7403eab4bd69513
EEEEEEcho <814466057@qq.com>
2022-06-05 19:34:13 +0800

    master commit2

--- src/main/java/chapter07/GitTest.java
+++ src/main/java/chapter07/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
         System.out.println("Hello git2");
         System.out.println("Hello git4");
 
-        System.out.println("Hello hot-fix");
+        System.out.println("Hello master test");
 
     }
 }
